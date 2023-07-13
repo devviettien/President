2198d687ac3915b433a6d37d513a68c31f37d534
devviettien <devbuinhathoang@gmail.com>
2023-07-13 18:21:48 +0700

    Initial Commit

---
+++ President/PresidentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresidentApp.swift
+//  President
+//
+//  Created by Nhật Hoàng on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PresidentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
